adc78b4b149adde73873f4c9895e19553a60afd4
Erik Onarheim <erik.onarheim@gmail.com>
2025-01-16 09:33:24 -0600

    treat tiled tsx files as xml instead of react

--- init.lua
+++ init.lua
@@ -1125,6 +1125,11 @@ require("lazy").setup({
 		},
 	},
 })
+vim.filetype.add({
+	extension = {
+		tsx = "xml", -- Treat .tsx files as XML
+	},
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
