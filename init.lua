529ea69c98a949cced4c4257cade01b17a13f8d9
Erik Onarheim <erik.onarheim@gmail.com>
2025-03-04 09:19:56 -0600

    update to add yazi

--- init.lua
+++ init.lua
@@ -256,24 +256,62 @@ require("lazy").setup({
 		},
 		config = function()
 			require("telescope").load_extension("lazygit")
-			vim.keymap.set("n", "<leader>gg", ":LazyGit<ENTER>")
+			vim.keymap.set("n", "<leader>lg", ":LazyGit<ENTER>")
 		end,
 	},
 	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			{ "nvim-tree/nvim-web-devicons" },
+		"mikavilpas/yazi.nvim",
+		event = "VeryLazy",
+		dependencies = { "folke/snacks.nvim", lazy = true },
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			{
+				"<leader>-",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				"<c-up>",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
 		},
-		config = function()
-			require("nvim-tree").setup({
-				git = {
-					enable = false,
-				},
-			})
+		opts = {
+			-- if you want to open yazi instead of netrw, see below for more info
+			open_for_directories = false,
+			keymaps = {
+				show_help = "<f1>",
+			},
+		},
+		-- 👇 if you use `open_for_directories=true`, this is recommended
+		init = function()
+			-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+			-- vim.g.loaded_netrw = 1
+			vim.g.loaded_netrwPlugin = 1
 		end,
 	},
+	-- {
+	-- 	"nvim-tree/nvim-tree.lua",
+	-- 	version = "*",
+	-- 	lazy = false,
+	-- 	dependencies = {
+	-- 		{ "nvim-tree/nvim-web-devicons" },
+	-- 	},
+	-- 	config = function()
+	-- 		require("nvim-tree").setup({
+	-- 			git = {
+	-- 				enable = false,
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 	-- Here is a more advanced example where we pass configuration
 	-- options to `gitsigns.nvim`. This is equivalent to the following Lua:
 	--    require('gitsigns').setup({ ... })
