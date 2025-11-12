84873c37cbe3bfdfa512e8f50162789d34f8dffc
Erik Onarheim <erik.onarheim@gmail.com>
2025-11-12 08:50:07 -0600

    tweak prefs

--- lua/preferences.lua
+++ lua/preferences.lua
@@ -13,7 +13,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.mouse = "a"
 vim.o.showmode = false
-vim.o.winborder = 'rounded'
+-- vim.o.winborder = 'rounded'
 
 vim.schedule(function()
 	vim.o.clipboard = "unnamedplus"
